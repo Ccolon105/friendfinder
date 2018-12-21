8ba968e0e3a6674261fa2f174acc39c210da3b58
Lawgiver17 <ccolon105@yahoo.com>
2018-12-20 20:57:10 -0700

    got the app working

---
+++ app/data/friends.js
@@ -0,0 +1,73 @@
+var friends = [
+	{
+	  "name":"Jon Snow",
+	  "photo":"https://upload.wikimedia.org/wikipedia/en/f/f0/Jon_Snow-Kit_Harington.jpg",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+	   ]
+	},
+	{
+	  "name":"Daenerys Targaryen",
+	  "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Daenerys_Targaryen_with_Dragon-Emilia_Clarke.jpg/330px-Daenerys_Targaryen_with_Dragon-Emilia_Clarke.jpg",
+	
+	  "scores":[
+	     1,
+	     2,
+	     5,
+	     5,
+	     1,
+	     2,
+	     3,
+	     1,
+	     5,
+	     2
+	   ]
+	},
+	{
+	  "name":"Sansa Stark",
+	  "photo":"https://upload.wikimedia.org/wikipedia/en/7/74/SophieTurnerasSansaStark.jpg",
+	 
+	  "scores":[
+	     4,
+	     5,
+	     3,
+	     3,
+	     4,
+	     5,
+	     1,
+	     4,
+	     3,
+	     5
+	   ]
+	},
+	{
+	  "name":"Tormund Giantsbane",
+	  "photo":"https://upload.wikimedia.org/wikipedia/en/6/6f/Tormund_Giantsbane-Kristofer_Hivju.jpg",
+	  
+	  "scores":[
+	     2,
+	     3,
+	     1,
+	     1,
+	     2,
+	     3,
+	     4,
+	     2,
+	     1,
+	     3
+	   ]
+	}
+];
+
+
+module.exports = friends;
+  
